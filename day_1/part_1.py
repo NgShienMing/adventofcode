24bbf60853c05a3d241a65c76fd283bcdc7fbd72
Ng Shien Ming <nsming1030@gmail.com>
2023-12-06 09:26:42 +0800

    day 1 solution

---
+++ day_1/part_1.py
@@ -0,0 +1,19 @@
+puzzle = []
+with open('part_1.txt') as f:
+    puzzle = f.read().splitlines()
+
+numbers = []
+for text in puzzle:
+    curr_num_list = []
+    num = ''
+    for char in text:
+        try:
+            curr_num_list.append(char)
+            int(char)
+        except Exception as e:
+            curr_num_list = curr_num_list[:-1]
+            continue
+    num += curr_num_list[0] + curr_num_list[-1]
+    numbers.append(int(num))
+
+print(sum(numbers))
---
+++ day_1/part_2.py
@@ -0,0 +1,64 @@
+valid_num_str = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+puzzle = []
+with open('part_2.txt') as f:
+    puzzle = f.read().splitlines()
+
+numbers = []
+for text in puzzle:
+    num = ''
+
+    curr_num_list = []
+    num_str = ''
+    for i in range(len(text)):
+        num_str = ''
+        for j in range(i, len(text)):
+            if j == i:
+                try:
+                    curr_num_list.append(text[j])
+                    int(text[j])
+                except Exception as e:
+                    curr_num_list = curr_num_list[:-1]
+
+                if len(curr_num_list) != 0:
+                    num += text[j]
+                    break
+
+            num_str += text[j]
+            if num_str in valid_num_str:
+                num += str(valid_num_str.index(num_str) + 1)
+                break
+
+        if num_str in valid_num_str or len(curr_num_list) != 0:
+            break
+
+
+    curr_num_list = []
+    num_str = ''
+    for i in range(len(text)-1, -1, -1):
+        num_str = ''
+        for j in range(i, -1, -1):
+            if j == i:
+                try:
+                    curr_num_list.append(text[j])
+                    int(text[j])
+                except Exception as e:
+                    curr_num_list = curr_num_list[:-1]
+
+                if len(curr_num_list) != 0:
+                    num += curr_num_list[0]
+                    break
+
+            num_str = text[j] + num_str
+            if num_str in valid_num_str:
+                num += str(valid_num_str.index(num_str) + 1)
+                break
+
+        if num_str in valid_num_str or (len(curr_num_list) != 0 and len(num) == 2):
+            break
+
+
+    numbers.append(int(num))
+
+
+print(sum(numbers))
